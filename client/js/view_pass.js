74f28bccc1a8b77f208efdd2a2bca39fa5d97b98
Lucas Silva <Lucassilvacode@gmail.com>
2023-04-10 08:36:48 -0300

    add pagina e logica para visualisação de passagens

---
+++ client/js/view_pass.js
@@ -0,0 +1,32 @@
+let container = document.querySelector('.conteiner')
+const dados = JSON.parse(localStorage.getItem('data'))
+
+function load_passagens() {
+    fetch('http://127.0.0.1:8000/voos/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'origem': dados.origem,
+            'destino': dados.destino,
+        })
+        })
+        .then(response => response.json())
+        .then(data => readPagePassagens(data))
+        .catch(error => console.log(error))
+}
+
+
+function readPagePassagens(dados) {
+    for (valor of dados) {
+        container.innerHTML +=
+    `<div>
+        <h3> viage de ${valor.origem} para ${valor.destino}</h3>
+        <span><p></p></span>
+        <p>preço ${valor.preco}</p>
+    </div>`
+    }
+}
+
+load_passagens()
