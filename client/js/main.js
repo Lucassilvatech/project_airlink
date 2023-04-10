ab7c2ad8deba649269d1dc4c7b95bd019c977180
Lucas Silva <Lucassilvacode@gmail.com>
2023-04-10 08:47:30 -0300

    estrutura e logica pagina principal

--- client/js/main.js
+++ client/js/main.js
@@ -1,27 +1,118 @@
-const token = localStorage.getItem('token')
-if (token == null){
-    window.location.href = '../pages/login.html'
-}
+let passagens = document.querySelector('.passagens')
+let origem = document.querySelector('.origem')
+let destino = document.querySelector('.destino')
+let carrinho = document.querySelector('span')
+let viewCarrinho = document.querySelector('.carrinho')
 
-window.onload = function(){
-let request = new XMLHttpRequest();
-request.onload = function(){
-    let my_obj = this.responseText;
-    let response = JSON.parse(my_obj);
-    console.log(response)
-    loadResponse(response)
 
-}
-request.open("GET",`http://127.0.0.1:8000/client/singin/permission?token=${token}`);
-request.send()
-}
-function loadResponse(response){
-if (response.error == 'token_not_exist'){
-window.location.href = '../pages/login.html'
-} 
-}
-
-function sair(){
+function sair() {
     localStorage.removeItem('token')
-    window.location.href = '../pages/index.html' 
+    window.location.href = '../pages/index.html'
 }
+
+
+fetch('http://127.0.0.1:8000/voos', {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    })
+    .then(response => response.json())
+    .then(data => loadPassagensAuto(data))
+    .catch(error => console.log(error))
+
+
+function loadPassagensAuto(data) {
+    let client = localStorage.getItem('nome')
+    document.querySelector('header').innerHTML = 'Olá, '+ client  
+    for (valor of data) {
+        passagens.innerHTML +=
+    `<div id="sub_box">
+        <h3> viage de ${valor.origem} para ${valor.destino}</h3>
+        <span><p></p></span>
+        <p>preço ${valor.preco}</p>
+        <p>Numero do voo:${valor.numero_voo}</p>
+        <div class="product" id="${valor.numero_voo}">Add Carrinho</div>
+    </div>`
+    }
+}
+
+
+function passagensSearch() {
+    let data = JSON.stringify({
+        'origem': origem.value,
+        'destino': destino.value,
+    })
+    
+    localStorage.setItem('data', data)
+    window.location.href = '../pages/view_passagens.html'
+}
+
+
+passagens.addEventListener('click', function(evt){
+    try{
+        let origin_value = evt.target.closest('.product').id
+        if (evt.target.id == 'sub_box'){
+            return;
+        }
+    let user = localStorage.getItem('user_id')
+    console.log(user)
+    fetch(`http://127.0.0.1:8000/client/carrinho/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'numero_voo': origin_value,
+            'user': user
+        })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }catch (error){
+        return
+    }
+})
+
+
+let cont = 1
+
+carrinho.addEventListener('click', () =>{
+
+    let user = localStorage.getItem('user_id')
+    fetch(`http://127.0.0.1:8000/search/carrinho/${user}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        })
+        .then(response => response.json())
+        .then(data => addCarrinho(data))
+        .catch(error => console.log(error))
+
+    viewCarrinho.classList.remove('noneDisplay')
+})
+
+
+viewCarrinho.addEventListener('click', (evt) =>{
+    if (evt.target.id == 'i'){
+        viewCarrinho.classList.add('noneDisplay')
+        viewCarrinho.innerHTML = '<i id="i">x</i>'
+    } 
+})
+
+
+function addCarrinho(data){
+    // carrinho.innerHTML = `Carrinho ${cont++}`
+    for (valor of data) {
+        viewCarrinho.innerHTML +=
+    `<div class="car_box">
+        <h3> viage de ${valor[0].origem} para ${valor[0].destino}</h3>
+        <span><p></p></span>
+        <p>preço ${valor[0].preco}</p>
+        <p>Numero do voo:${valor[0].numero_voo}</p>
+    </div>`
+    }
+}
+
